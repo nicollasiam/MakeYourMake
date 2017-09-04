df6485370a01ff91e9b895c9dbf1cc9cb6b94ef5
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-04 14:22:40 -0300

    Created Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904172028) do
+ActiveRecord::Schema.define(version: 20170904172206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 20170904172028) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_makes_on_user_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "score"
+    t.string   "title"
+    t.text     "content"
+    t.integer  "make_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["make_id"], name: "index_reviews_on_make_id", using: :btree
   end
 
   create_table "types", force: :cascade do |t|
@@ -76,4 +86,5 @@ ActiveRecord::Schema.define(version: 20170904172028) do
   add_foreign_key "make_types", "makes"
   add_foreign_key "make_types", "types"
   add_foreign_key "makes", "users"
+  add_foreign_key "reviews", "makes"
 end

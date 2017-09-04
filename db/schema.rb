73c5b986dd577137b8be0395f8cadb047e7140de
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-04 14:17:49 -0300

    Created Image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904171452) do
+ActiveRecord::Schema.define(version: 20170904171711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "make_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["make_id"], name: "index_images_on_make_id", using: :btree
+  end
 
   create_table "makes", force: :cascade do |t|
     t.string   "name"
@@ -50,5 +57,6 @@ ActiveRecord::Schema.define(version: 20170904171452) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "images", "makes"
   add_foreign_key "makes", "users"
 end

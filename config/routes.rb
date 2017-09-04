9e06504def7895560b8bfd276c10e190a57a99ae
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-04 14:26:38 -0300

    Create Makes controller, with all views. Created Routes to makes and changed root to makes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'makes#index'
+
+
+
+  resources :makes
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

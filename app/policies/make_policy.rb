53ed592152d2cab3d80476ae26c57de059fed37e
Bruno Cerqueira <brunocerqueiram@gmail.com>
2017-09-05 18:44:45 -0300

    Ajustando pundit

--- app/policies/make_policy.rb
+++ app/policies/make_policy.rb
@@ -4,4 +4,22 @@ class MakePolicy < ApplicationPolicy
       scope
     end
   end
+
+  def create?
+    return true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+  end
+
+  def destroy?
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    record.user == user
+  end
 end

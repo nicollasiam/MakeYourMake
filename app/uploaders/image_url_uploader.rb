c5c70dba60e75beecf823fbfa6452b8e6c55807d
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-05 11:36:06 -0300

    Created image_url column in Image model. Deleted imageUrl uploader and created Image Uploader (just to have a better name)

--- app/uploaders/image_url_uploader.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-class ImageUrlUploader < CarrierWave::Uploader::Base
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904172206) do
+ActiveRecord::Schema.define(version: 20170905143214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170904172206) do
     t.integer  "make_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
     t.index ["make_id"], name: "index_images_on_make_id", using: :btree
   end
 

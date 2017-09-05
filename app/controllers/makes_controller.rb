c8e9ccbf029468c13d3461432b29cd9aabb777cd
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-05 19:57:10 -0300

    Added the namespace profile/makes, to be acessible only to user.

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -9,44 +9,4 @@ class MakesController < ApplicationController
   def show
     @make = Make.find(params[:id])
   end
-
-  def edit
-    @make = Make.find(params[:id])
-  end
-
-  def new
-    @make = Make.new
-  end
-
-  def create
-    make = Make.new(make_params)
-    make.likes_count = 0
-    make.user = current_user
-
-    if make.save!
-      redirect_to makes_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    make = Make.find(params[:id])
-
-    if make.update(make_params)
-      redirect_to make_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-  end
-
-  # Private methods
-  private
-
-  def make_params
-    params.require(:make).permit(:name, :description, :likes_count, :public)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,14 @@
 Rails.application.routes.draw do
-
-  namespace :profile do
-    get 'makes/index'
-  end
-
-  namespace :profile do
-    get 'makes/show'
-  end
-
-  namespace :profile do
-    get 'makes/edit'
-  end
-
-  namespace :profile do
-    get 'makes/new'
-  end
-
   root to: 'makes#index'
 
+  namespace :profile do
+    resources :makes
+  end
 
+  resources :makes, only: [:index, :show]
+
+  # DELETE?
   resources :images, only: [:new, :create]
-  resources :makes
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

729ff7413eb9df6f396f4fad7f9a9b0837a8ab31
Bruno Cerqueira <brunocerqueiram@gmail.com>
2017-09-06 10:02:22 -0300

    Atualizadas políticas

--- app/policies/make_policy.rb
+++ app/policies/make_policy.rb
@@ -1,7 +1,7 @@
 class MakePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
   end
 
@@ -9,17 +9,17 @@ class MakePolicy < ApplicationPolicy
     return true
   end
 
-  def update?
-    user_is_owner_or_admin?
-  end
+  # def update?
+  #   user_is_owner_or_admin?
+  # end
 
-  def destroy?
-    user_is_owner_or_admin?
-  end
-
-  private
-
-  def user_is_owner_or_admin?
-    record.user == user
-  end
+#   def destroy?
+#     user_is_owner_or_admin?
+#   end
+#
+#   private
+#
+#   def user_is_owner_or_admin?
+#     record.user == user || user.admin
+#   end
 end

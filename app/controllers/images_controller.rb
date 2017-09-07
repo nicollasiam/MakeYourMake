b4297dd6718f68b4c0c0b0cea63c75aac80ec042
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-07 13:11:19 -0300

    Added size validator to the image (less than 500kb)

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -12,14 +12,19 @@ class ImagesController < ApplicationController
 
   def create
     # raise
-    make = Make.find(params[:make_id])
+    @make = Make.find(params[:make_id])
 
-    if make.user == current_user
+    if @make.user == current_user
       image = Image.new(image_params)
-      image.make = make
+      image.make = @make
       authorize(image)
 
-      image.save!
+      if image.save
+        redirect_to make_path(@make)
+      else
+        # TODO: Stay in this view and show an error message
+        redirect_to makes_path
+      end
     else
       # TODO: Redirect to 401 page (unauthorized)
     end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,11 @@
+require 'file_size_validator'
+
 class Image < ApplicationRecord
   belongs_to :make
 
   mount_uploader :image_url, ImageUploader
 
-  validates :image_url, presence: true
+  validates :image_url, presence: true, file_size: { maximum: 0.5.megabytes.to_i, message: 'O tamanho da imagem não deve ultrapassar 500kb' }
 end
 
 

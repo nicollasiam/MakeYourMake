fc9d9e4f62a17c705e82dc0fb9e53439b4252d9b
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-08 17:18:26 -0300

    Added card to index and started doing cards for the type page

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,6 +1,9 @@
 class TypesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
     type = Type.find(params[:id])
-    @makes = policy_scope(Make).where(type: type)
+    @makes = policy_scope(Make).joins(:make_types).where(make_types: { type: type })
+    authorize(@makes)
   end
 end

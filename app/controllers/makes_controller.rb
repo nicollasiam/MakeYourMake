a246bd08a596f0f8527ae68b0d9130b1949f67d1
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-12 14:31:38 -0300

    Added a simple input box on top of index page. Performed the search using like.

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -3,7 +3,17 @@ class MakesController < ApplicationController
 
 
   def index
-    # @feautured_makes = policy_scope(Make).order(likes_count: :desc).limit(9)
+    if params[:search_text].present?
+      # Search for makes with given description
+      @makes = Make.where('description LIKE ?', "%#{params[:search_text]}%")
+
+      # It is possible that the user is searching for a professional
+      # Search for professional if no makes were found
+      if @makes.length.zero?
+        @users = User.where('artistic_name LIKE ?', "%#{params[:search_text]}%").where(professional: :true)
+      end
+    end
+
     @types = policy_scope(Type).sample(9)
   end
 

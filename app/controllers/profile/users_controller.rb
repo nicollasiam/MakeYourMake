60cef4e4c05ceca4c29246fabd30652aa3dbd2fc
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-09-21 07:56:36 -0300

    Reincluded css for profile page

--- app/controllers/profile/users_controller.rb
+++ app/controllers/profile/users_controller.rb
@@ -2,9 +2,11 @@ class Profile::UsersController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :show
   skip_after_action :verify_authorized, :only => :show
   skip_after_action :verify_policy_scoped, :only => :show
+
   def show
     @user = User.friendly.find(params[:id])
-    @makes = Make.friendly.where(["user_id = ?", @user.id])
+    @makes = Make.where(user: @user)
+
     @banner_makes = @makes.select do |make|
       make.images.first.present?
     end

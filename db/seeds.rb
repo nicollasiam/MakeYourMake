b77692bdd14e0382a6f80c3b978be667747ae12c
Luana Perin <luanakperin@gmail.com>
2017-10-09 14:29:16 -0300

    alterações no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@ types << Type.create!(name: "Noite")
 types << Type.create!(name: "Festa")
 types << Type.create!(name: "Colorida")
 types << Type.create!(name: "Neutra")
+types << Type.create!(name: "Iluminada")
 types << Type.create!(name: "Artistica")
 types << Type.create!(name: "Casamento")
+types << Type.create!(name: "Inspirações")
 
 # Admin User
 User.create!(email: 'makeyourmaketeste@gmail.com', first_name: 'Admin',
-            last_name: 'Adminteste', artistic_name: 'Make Admin',
+            last_name: 'Adminorino', artistic_name: 'Make Admin',
             password: 'poihd5rh4', admin: true, professional: false)
-
-
 
